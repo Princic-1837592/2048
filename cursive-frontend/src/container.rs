09627448da0f09a13945e296fa573fdabda3d4d4
Princic-1837592 <andreapri.99@gmail.com>
2023-09-24 11:21:53 +0200

    added history slider, instructions

--- cursive-frontend/src/container.rs
+++ cursive-frontend/src/container.rs
@@ -2,14 +2,15 @@ use backend::{Direction, Game};
 use cursive::{
     event::{AnyCb, Event, EventResult, Key},
     theme::{Color, ColorStyle, PaletteColor},
-    traits::{Nameable, Resizable},
+    traits::{Nameable, Resizable, Scrollable},
     view::{Selector, SizeConstraint},
-    views::{Dialog, LinearLayout, SliderView},
+    views::{Dialog, LinearLayout, SliderView, TextView},
     Printer, Vec2, View,
 };
 use lazy_static::lazy_static;
 
 const HISTORY_WIDTH: usize = 13;
+const HISTORY_HEIGHT: usize = 7;
 const SCORE_WIDTH: usize = 11;
 const SCORE_HEIGHT: usize = 3;
 const INTER_SPACE: usize = 2;
@@ -40,16 +41,20 @@ pub(crate) struct Container {
     sliders: LinearLayout,
     next_width: usize,
     next_height: usize,
+    next_history: usize,
 }
 
 impl Container {
     pub(crate) fn new() -> Container {
         let game = Game::default();
+        let (game_width, game_height, game_history) =
+            (game.width(), game.height(), game.max_history());
+
         let mut sliders = LinearLayout::vertical();
         sliders.add_child(
             Dialog::around(
                 SliderView::horizontal((backend::MIN_SIZE..=backend::MAX_SIZE).count())
-                    .value(backend::DEFAULT_SIZE - backend::MIN_SIZE)
+                    .value(game_width - backend::MIN_SIZE)
                     .on_change(|s, v| {
                         let title = format!("Width: {}", v + backend::MIN_SIZE);
                         s.call_on_name("width", |view: &mut Dialog| view.set_title(title));
@@ -58,13 +63,13 @@ impl Container {
                         });
                     }),
             )
-            .title(format!("Width: {}", backend::DEFAULT_SIZE))
+            .title(format!("Width: {}", game_width))
             .with_name("width"),
         );
         sliders.add_child(
             Dialog::around(
                 SliderView::horizontal((backend::MIN_SIZE..=backend::MAX_SIZE).count())
-                    .value(backend::DEFAULT_SIZE - backend::MIN_SIZE)
+                    .value(game_height - backend::MIN_SIZE)
                     .on_change(|s, v| {
                         let title = format!("Height: {}", v + backend::MIN_SIZE);
                         s.call_on_name("height", |view: &mut Dialog| view.set_title(title));
@@ -73,14 +78,30 @@ impl Container {
                         });
                     }),
             )
-            .title(format!("Height: {}", backend::DEFAULT_SIZE))
+            .title(format!("Height: {}", game_height))
             .with_name("height"),
+        );
+        sliders.add_child(
+            Dialog::around(
+                SliderView::horizontal(6)
+                    .value(game_history)
+                    .on_change(|s, v| {
+                        let title = format!("History: {}", v);
+                        s.call_on_name("history", |view: &mut Dialog| view.set_title(title));
+                        s.call_on_name("container", |view: &mut Container| {
+                            view.next_history = v;
+                        });
+                    }),
+            )
+            .title(format!("History: {}", game_history))
+            .with_name("history"),
         );
         Container {
             game,
             sliders,
             next_width: backend::DEFAULT_SIZE,
             next_height: backend::DEFAULT_SIZE,
+            next_history: 1,
         }
     }
 
@@ -162,19 +183,20 @@ impl Container {
             .game
             .history()
             .iter()
+            .rev()
             .map(|h| format!("{:?}", h))
             .collect::<Vec<_>>()
             .join("\n");
-        let mut dialog = Dialog::text(if history.is_empty() {
-            "no history"
-        } else {
-            &history
-        })
+        let mut dialog = Dialog::around(
+            TextView::new(if history.is_empty() {
+                "no history"
+            } else {
+                &history
+            })
+            .scrollable(),
+        )
         .title("History")
-        .resized(
-            SizeConstraint::Fixed(HISTORY_WIDTH),
-            SizeConstraint::AtMost(7),
-        );
+        .fixed_size((HISTORY_WIDTH, HISTORY_HEIGHT));
         dialog.layout(printer.offset);
         dialog.draw(printer);
     }
@@ -191,6 +213,15 @@ impl Container {
         self.sliders.draw(printer);
     }
 
+    fn draw_instructions(&self, printer: &Printer) {
+        let mut dialog =
+            Dialog::text("wasd or arrow keys to move\nn to start a new game\nz to undo\nq to quit")
+                .title("Instructions")
+                .resized(SizeConstraint::Free, SizeConstraint::Fixed(6));
+        dialog.layout(printer.offset);
+        dialog.draw(printer);
+    }
+
     fn width(&self) -> usize {
         self.game.width() * (CELL_EXT_WIDTH - 1) + 1
     }
@@ -199,29 +230,42 @@ impl Container {
         self.game.height() * (CELL_EXT_HEIGHT - 1) + 1
     }
 
-    fn offsets(&self) -> (Vec2, Vec2, Vec2, Vec2) {
+    fn offsets(&self) -> (Vec2, Vec2, Vec2, Vec2, Vec2) {
         let (x, y) = (OUTER_SPACE, OUTER_SPACE);
-        let board_padding = Vec2::new(x, y);
-        let history_padding = Vec2::new(board_padding.x + self.width() + INTER_SPACE, y);
-        let score_offset = Vec2::new(history_padding.x + HISTORY_WIDTH + INTER_SPACE, y);
+        let board_offset = Vec2::new(x, y);
+        let history_offset = Vec2::new(board_offset.x + self.width() + INTER_SPACE, y);
+        let score_offset = Vec2::new(history_offset.x + HISTORY_WIDTH + INTER_SPACE, y);
         let sliders_offset = Vec2::new(score_offset.x, score_offset.y + SCORE_HEIGHT);
-        (board_padding, history_padding, score_offset, sliders_offset)
+        let instructions_offset = Vec2::new(
+            history_offset.x,
+            sliders_offset.y + HISTORY_HEIGHT + INTER_SPACE,
+        );
+        (
+            board_offset,
+            history_offset,
+            score_offset,
+            sliders_offset,
+            instructions_offset,
+        )
     }
 }
 
 impl View for Container {
     fn draw(&self, printer: &Printer) {
-        let (board_offset, history_offset, score_offset, sliders_offset) = self.offsets();
+        let (board_offset, history_offset, score_offset, sliders_offset, instructions_offset) =
+            self.offsets();
 
         let board_printer = printer.offset(board_offset);
         let history_printer = printer.offset(history_offset);
         let score_printer = printer.offset(score_offset);
         let sliders_printer = printer.offset(sliders_offset);
+        let instructions_printer = printer.offset(instructions_offset);
 
         Container::draw_board(self, &board_printer);
         Container::draw_history(self, &history_printer);
         Container::draw_score(self, &score_printer);
         Container::draw_sliders(self, &sliders_printer);
+        Container::draw_instructions(self, &instructions_printer);
     }
 
     fn layout(&mut self, constraints: Vec2) {
@@ -257,7 +301,8 @@ impl View for Container {
                 self.game.push(Direction::R);
             }
             Event::Char('n') => {
-                self.game = Game::new(self.next_width, self.next_height, 1).unwrap_or_default();
+                self.game = Game::new(self.next_width, self.next_height, self.next_history)
+                    .unwrap_or_default();
             }
             Event::Char('z') => {
                 self.game.undo();
